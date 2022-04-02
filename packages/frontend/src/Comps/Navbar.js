8501af51c59165e203b5fb449377b28acc96e0a7
Mateja <maxon4002@gmail.com>
2022-04-02 13:45:39 +0200

    First Commit

---
+++ packages/frontend/src/Comps/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+    <div className="w-full h-9 bg-slate-300">
+        <div>
+            <input type="text" />
+        </div>
+        <div>
+            LOGO
+        </div>
+        <div>
+            <button>Register</button>
+            <button>Login in</button>
+            <button>Cart</button>
+
+        </div>
+    </div>);
+}
