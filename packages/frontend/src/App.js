ba57e0fcacd34e951cece34f325c49d9f5d5bf46
svetozar12 <zaauna@abv.bg>
2022-04-10 20:25:31 +0300

    fixed some ui and ux issues

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MOCK_DATA from "./Data/MOCK_DATA";
+// components
 import Navbar from "./Components/Navbar";
 import Carousel from "./Components/Carousel";
 import Newsletter from "./Components/Newsletter";
 import Footer from "./Components/Footer";
-import MOCK_DATA from "./Data/MOCK_DATA";
 import DisplayItems from "./Components/Seasons";
+// pages
 import DisplayProducts from "./Pages/DisplayProducts";
-import Login from "./Pages/Login_Signin/Login";
-import Log_Sign from "./Pages/Login_Signin/Log_Sign";
+import Login from "./Pages/IndexPage/Login";
+import IndexPage from "./Pages/IndexPage/IndexPage";
 
 function App() {
   return (
     <>
-    {/* <Log_Sign/> */}
+      <Link to="/">
+        <IndexPage />
+      </Link>
       <Navbar />
       <main className="flex flex-col  items-center justify-start w-screen overflow-x-hidden min-h-screen  w-max-full bg-yellow-200">
         {/* {/* <Carousel /> */}
         <DisplayItems />
         <Newsletter />
-        <Footer /> 
+        <Footer />
         Displaying all the products
         {/* <DisplayProducts /> */}
       </main>
--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,21 +1,27 @@
+import "./index.css";
+// deps
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import App from "./App";
+// config
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+// components
 import Navbar from "./Components/Navbar";
 import Newsletter from "./Components/Newsletter";
 import Footer from "./Components/Footer";
 import DisplayItems from "./Components/Seasons";
-import Login from "./Pages/Login_Signin/Login";
-import Signin from "./Pages/Login_Signin/Signin";
-import App from "./App";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
+import Login from "./Pages/IndexPage/Login";
+import Signin from "./Pages/IndexPage/Signin";
 
 ReactDOM.render(
-  <Provider store={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root"),
 );
