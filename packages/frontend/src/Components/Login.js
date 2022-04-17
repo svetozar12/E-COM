58298805dd3786b8effa5b677f86dc7efce76744
Mateja <maxon4002@gmail.com>
2022-04-17 13:40:05 +0200

    Minor Update

--- packages/frontend/src/Components/Login.js
+++ packages/frontend/src/Components/Login.js
@@ -5,6 +5,13 @@ import {
   AiOutlineEye,
 } from "react-icons/ai";
 import { ExternalLink } from "react-external-link";
+// Auth
+import axios from "axios";
+import Cookies from "js-cookie";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { ActionType } from "../redux/types";
+
 
 const Login = ({ wantsLogIn, setWantsLogIn }) => {
   const [showPassword, setShowPassword] = useState(false);
@@ -15,9 +22,16 @@ const Login = ({ wantsLogIn, setWantsLogIn }) => {
   const [isValidEmail, setIsValidEmail] = useState(true);
   const [isPasswordShort, setIsPasswordShort] = useState(false);
 
+
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [message, setMessage] = useState('')
   // React.useEffect(() => {
   //     console.log(passwordValue)
   // }, [passwordValue])
+
+
 
   useEffect(() => {
     let timeout = setTimeout(() => {
@@ -33,24 +47,55 @@ const Login = ({ wantsLogIn, setWantsLogIn }) => {
     return () => clearTimeout(timeout);
   }, [isPasswordShort]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // EMAIL VALIDATION
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(emailValue)) {
-      console.log("valid email");
-    } else {
-      console.log("email not valid!");
-      setIsValidEmail(false);
+    try {
+      // EMAIL VALIDATION
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (re.test(emailValue)) {
+        console.log("valid email");
+      } else {
+        console.log("email not valid!");
+        setIsValidEmail(false);
+      }
+
+      if (passwordValue.length <= 4) {
+        setIsPasswordShort(true);
+        return false;
+      }
+      dispatch({ type: ActionType.SET_LOADING })
+      // INVALID USER MESSAGE INSTEAD OF LOADING WHEN USER DOESNT EXIST!
+
+      if (emailValue && passwordValue) {
+
+        const res = await axios.post(
+          "http://localhost:5000/auth/login",
+          {
+            email: emailValue,
+            password: passwordValue,
+          }
+        )
+        Cookies.set("token", res.data.Access_token, { expires: 3600 });
+
+        navigate("./home", { replace: true });
+        setEmailValue("");
+        setPasswordValue("");
+
+        dispatch({ type: ActionType.LOGGED_IN })
+        dispatch({ type: ActionType.REMOVE_LOADING })
+
+        return true
+      }
+
     }
 
-    if (passwordValue.length <= 4) {
-      setIsPasswordShort(true);
-    } else if (passwordValue.length > 10) {
+    catch (error) {
+      setMessage(error)
+      console.log(message)
+
     }
 
-    console.log("submited");
   };
 
   return (
@@ -58,7 +103,9 @@ const Login = ({ wantsLogIn, setWantsLogIn }) => {
       onSubmit={handleSubmit}
       className="w-2/3 h-3/4 flex flex-col justify-center gap-6 items-center "
     >
+
       <div className=" md:w-72 w-11/12   h-2/4 flex flex-col  justify-center gap-8 items-center ">
+        {/* <span className="text-red-600 text-xl">{message}</span> */}
         <div className="relative w-full h-1/4 ">
           <input
             autoComplete="off"
@@ -75,7 +122,7 @@ const Login = ({ wantsLogIn, setWantsLogIn }) => {
           {emailValue ? (
             ""
           ) : (
-            <span className="absolute top-2 md:top-2 lg:top-3 right-2 text-gray-400 text-3xl">
+            <span className="absolute top-2 md:top-2 lg:top-3.5 right-2 text-gray-400 text-3xl">
               <AiOutlineMail />
             </span>
           )}
@@ -100,7 +147,7 @@ const Login = ({ wantsLogIn, setWantsLogIn }) => {
           />
 
           <span
-            className="absolute cursor-pointer top-2 md:top-2 lg:top-3 right-2 text-gray-400 text-3xl"
+            className="absolute cursor-pointer top-2 md:top-2 lg:top-3.5 right-2 text-gray-400 text-3xl"
             onClick={() => setShowPassword(!showPassword)}
           >
             {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
--- packages/frontend/src/redux/reducers/setReducer.js
+++ packages/frontend/src/redux/reducers/setReducer.js
@@ -1,13 +1,27 @@
 import { ActionType } from "../types";
 
 export const initialState = {
+  name: '',
   isBottom: false,
   toggleNav: false,
+  LoggedIn: false,
+  isLoading: false,
+
 };
 const setReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.SET_BOTTOM:
       return { ...state, isBottom: !state.isBottom };
+    case ActionType.LOGGED_IN:
+      return { ...state, LoggedIn: true }
+    case ActionType.LOG_OUT:
+      return {...state,LoggedIn:false}
+    case ActionType.SAVE_NAME:
+      return {...state,name:action.payload}
+    case ActionType.SET_LOADING:
+      return {...state,isLoading: true}
+    case ActionType.REMOVE_LOADING:
+      return {...state,isLoading:false}
     default:
       return state;
   }
